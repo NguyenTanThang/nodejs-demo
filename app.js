105dae12a7c348dd129f4d92ce16655f9bdfc58b
Victor <nguyentanthang9a4@gmail.com>
2019-12-23 14:39:06 +0700

    Add in CORS

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var jwtValidator = require("./config/auth");
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var postsRouter = require('./routes/posts');
 
 var app = express();
+app.use(cors());
 require("./config/db");
 
 // view engine setup
