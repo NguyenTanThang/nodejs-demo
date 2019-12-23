366bb9acc685985138c0ed25e758b3bd99278038
Victor <nguyentanthang9a4@gmail.com>
2019-12-23 13:31:36 +0700

    Stop listening

--- app.js
+++ app.js
@@ -43,10 +43,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+/*
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Running on port " + PORT);
 })
+*/
 
 module.exports = app;
