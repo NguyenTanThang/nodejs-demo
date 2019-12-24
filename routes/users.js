b6a91c8fffe41789887eec65252eeb74f33a2e5c
Victor <nguyentanthang9a4@gmail.com>
2019-12-24 09:16:03 +0700

    Version 1.2.0

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../model/user");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  
+router.get('/users/:user_id', function(req, res, next) {
+    const user_id = req.params.user_id;
+
+    User.findById(user_id)
+    .then(user => {
+        res.json({
+            user
+        })
+    })
 });
 
 module.exports = router;
